e3d2d9a4a5c2430baf812726494b059fa3dd748c
ChrisBarnes01 <cgbarnes@stanford.edu>
2020-10-01 13:25:26 -0700

    Progress

--- src/components/NewHome/index.js
+++ src/components/NewHome/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import BootstrapTable from 'react-bootstrap-table-next';
 import './newHome.css'
+import { withFirebase } from '../Firebase';
+import { withRouter } from "react-router-dom";
 
 const monthNames = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
   "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
@@ -69,76 +71,194 @@ const columns = [{
 }];
 
 
-const NewHome = () => (
-  <div className='newHomeBody'>
-    <h1>{ getWelcomeMessage() }</h1>
-    <h3>Notificaciones</h3>
+class NewHome extends Component { 
 
-    <div className="whiteBackground">
-      <BootstrapTable keyField='id' 
-        data={ products } 
-        columns={ columns } 
-        rowEvents={ rowEvents }         
-        rowStyle={ {  } } 
-        hover={ true }
-        bordered={ false }
-      />
-    </div>
-    <br/>
-
-    <br/>
-    <h3>Calendario: { getDateString() }</h3>
-
-    <div className="calendarComponent">
-      <div className="calendarBox">
-        <p>{ getDayPlus(0) }</p>
-        <div className="picturesDue">
-          <p>Pictures Due</p>
-        </div>
-        <div className="appointment">
-          <p>Appointent</p>
-        </div>
-        
-
-      </div>
-      <div className="calendarBox">
-        <p>{ getDayPlus(1) }</p>
-        <div className="picturesDue">
-          <p>Pictures Due</p>
-        </div>
-
-      </div>
-
-      <div className="calendarBox">
-        <p>{ getDayPlus(2) }</p>
-        <div className="picturesDue">
-          <p>Pictures Due</p>
-        </div>
-        <div className="appointment">
-          <p>Appointent</p>
-        </div>
-
-      </div>
-      <div className="calendarBox">
-        <p>{ getDayPlus(3) }</p>
-
-      </div>
-      <div className="calendarBox">
-        <p>{ getDayPlus(4) }</p>
-
-      </div>
-      <div className="calendarBox">
-        <p>{ getDayPlus(5) }</p>
-
-      </div>
-      <div className="calendarBox">
-        <p>{ getDayPlus(6) }</p>
-
-      </div>
-    </div>
-
-
-  </div>
-);
+  constructor(props) {
+    super(props);
  
-export default NewHome;
+    this.state = {
+      loading: false,
+      notifications: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+ 
+    this.props.firebase.patients().on('value', snapshot => {
+      const usersObject = snapshot.val();
+      
+      /*const usersList = Object.keys(usersObject).map(key => ({
+        notification:key, 
+        datetime:usersObject[key].whatsAppNumber  
+      }));*/
+      var usersObjectKeys = Object.keys(usersObject); 
+      var notificationsList = [];
+      var calendarMap = new Map();
+      for (var i = 0; i < usersObjectKeys.length; i++){
+        var user = usersObject[usersObjectKeys[i]];
+        console.log(user)
+        if (user["hasBeenAccessed"]){
+          //Get Notifications for checkins
+          var checkInNotificationArray = user["checkInObjectList"]
+          var checkInKeys = Object.keys(checkInNotificationArray)
+          for (var j = 0; j < checkInKeys.length; j++){
+            var checkInObject = checkInNotificationArray[checkInKeys[j]]
+            if (!checkInObject["viewedNofication"]){
+              var notificationTemplate = user["firstName"] + " checked in";
+              var dateTemplate = checkInObject["epochTime"]
+              var date = new Date(dateTemplate)
+              const ye = new Intl.DateTimeFormat('es', { year: 'numeric' }).format(date);
+              const mo = new Intl.DateTimeFormat('es', { month: 'short' }).format(date);
+              const da = new Intl.DateTimeFormat('es', { day: '2-digit' }).format(date);
+              var toAddNotfication = {"notification":notificationTemplate, "datetime":`${da}-${mo}-${ye}` }
+              notificationsList.push(toAddNotfication)
+            }
+          }
+          //Get Notifications for Photos Uploaded
+          var photoNotificationArray = user["photoSetList"]
+          console.log("DOING PHOTOS ", user["firstName"])
+          var photoKeys = Object.keys(photoNotificationArray)
+          for (var k = 0; k < photoKeys.length; k++){
+            var photoObject = photoNotificationArray[photoKeys[k]]
+            console.log("PHOTO OBJECT IS")
+            console.log(photoObject)
+            console.log(photoObject["viewedNotification"])
+            if (!photoObject["viewedNotification"]){
+              var notificationTemplate = user["firstName"] + " uploaded a set of photos";
+              var dateTemplate = photoObject["epochTime"]
+              console.log(date)
+              var date = new Date(dateTemplate)
+              const ye = new Intl.DateTimeFormat('es', { year: 'numeric' }).format(date);
+              const mo = new Intl.DateTimeFormat('es', { month: 'short' }).format(date);
+              const da = new Intl.DateTimeFormat('es', { day: '2-digit' }).format(date);
+              var toAddNotfication = {"notification":notificationTemplate, "datetime":`${da}-${mo}-${ye}` }
+              notificationsList.push(toAddNotfication)
+            }
+          }
+          //Set Calendar for users
+          var calendarArray = user["calendarObjectList"];
+          console.log("calendarArray")
+          console.log(calendarArray);
+          for (var l = 0; l < calendarArray.length; l++){
+            console.log(calendarArray[l]);
+            var calendarEvent = calendarArray[l];
+            var calendarTime = calendarEvent["epochTime"]
+            console.log(calendarEvent)
+            var overallDate = new Date(calendarTime);
+            var year = overallDate.getFullYear();
+            console.log(year)
+            var day = calendarEvent["appointment_date"]
+            var type = calendarEvent["appointment_type"]
+            //Add year+day key to 
+            var key = day + year;
+            console.log("key", key);
+            //Check to see CalendarMapKey
+            var toAdd = "hello " + user["firstName"]
+            if (calendarMap[key] != undefined){
+              calendarMap[key].push(toAdd);
+            }
+            else{
+              calendarMap[key] = [toAdd]
+            }
+
+          }
+
+
+        }
+        else{
+          console.log("This user hasn't been checked")
+        }
+        
+      }
+      console.log(calendarMap)
+
+      console.log(notificationsList)
+
+      this.setState({
+        notifications: notificationsList,
+        loading: false,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.patients().off();
+  }
+
+  render() {
+    return (
+      <div className='newHomeBody'>
+        <h1>{ getWelcomeMessage() }</h1>
+        <h3>Notificaciones</h3>
+
+        <div className="whiteBackground">
+          <BootstrapTable keyField='id' 
+            data={ this.state.notifications } 
+            columns={ columns } 
+            rowEvents={ rowEvents }         
+            rowStyle={ {  } } 
+            hover={ true }
+            bordered={ false }
+          />
+        </div>
+        <br/>
+
+        <br/>
+        <h3>Calendario: { getDateString() }</h3>
+
+        <div className="calendarComponent">
+          <div className="calendarBox">
+            <p>{ getDayPlus(0) }</p>
+            <div className="picturesDue">
+              <p>Pictures Due</p>
+            </div>
+            <div className="appointment">
+              <p>Appointent</p>
+            </div>
+            
+
+          </div>
+          <div className="calendarBox">
+            <p>{ getDayPlus(1) }</p>
+            <div className="picturesDue">
+              <p>Pictures Due</p>
+            </div>
+
+          </div>
+
+          <div className="calendarBox">
+            <p>{ getDayPlus(2) }</p>
+            <div className="picturesDue">
+              <p>Pictures Due</p>
+            </div>
+            <div className="appointment">
+              <p>Appointent</p>
+            </div>
+
+          </div>
+          <div className="calendarBox">
+            <p>{ getDayPlus(3) }</p>
+
+          </div>
+          <div className="calendarBox">
+            <p>{ getDayPlus(4) }</p>
+
+          </div>
+          <div className="calendarBox">
+            <p>{ getDayPlus(5) }</p>
+
+          </div>
+          <div className="calendarBox">
+            <p>{ getDayPlus(6) }</p>
+
+          </div>
+        </div>
+
+
+      </div>
+    )
+  }
+};
+ 
+export default withRouter(withFirebase(NewHome));
